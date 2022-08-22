c257e3f702a3c309ca9ada96ad58f4dcd43dbaf2
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-08-22 16:56:10 +0900

    [dev branch] addTest

--- src/main/java/unit2/myTodoApp/v1/controller/TodoController.java
+++ src/main/java/unit2/myTodoApp/v1/controller/TodoController.java
@@ -2,12 +2,15 @@ package unit2.myTodoApp.v1.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import unit2.myTodoApp.v1.model.TodoEntity;
+import unit2.myTodoApp.v1.model.TodoRequest;
 import unit2.myTodoApp.v1.model.TodoResponse;
 import unit2.myTodoApp.v1.service.TodoService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -17,38 +20,60 @@ public class TodoController {
     private final TodoService service;
 
     @PostMapping
-    public ResponseEntity<TodoResponse> createTodo() {
+    public ResponseEntity<TodoResponse> createTodo(@RequestBody TodoRequest request) {
         System.out.println("CREATE");
-        return null;
+
+        if(ObjectUtils.isEmpty(request.getTitle())){
+            return ResponseEntity.badRequest().build();
+        }
+        if(ObjectUtils.isEmpty(request.getOrder())){
+            request.setOrder(0L);
+        }
+        if(ObjectUtils.isEmpty(request.getCompleted())) {
+            request.setCompleted(false);
+        }
+
+        TodoEntity result = this.service.add(request);
+        return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<TodoResponse> readOne() {
+    public ResponseEntity<TodoResponse> readOne(@PathVariable Long id) {
         System.out.println("READ ONE");
-        return null;
+
+        TodoEntity result = this.service.searchById(id);
+        return ResponseEntity.ok(new TodoResponse(result));
     }
 
     @GetMapping
-    public ResponseEntity<List<TodoEntity>> readAll() {
+    public ResponseEntity<List<TodoResponse>> readAll() {
         System.out.println("READ ALL");
-        return null;
+
+        List<TodoEntity> list = this.service.searchAll();
+        List<TodoResponse> result = list.stream().map(TodoResponse::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(result);
     }
 
     @PatchMapping("{id}")
-    public ResponseEntity<TodoEntity> update() {
+    public ResponseEntity<TodoEntity> update(@PathVariable Long id, @RequestBody TodoRequest request) {
         System.out.println("UPDATE");
-        return null;
+
+        TodoEntity result = this.service.updateById(id, request);
+        return ResponseEntity.ok(result);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteOne() {
+    public ResponseEntity<?> deleteOne(@PathVariable Long id) {
         System.out.println("DELETE ONE");
-        return null;
+        this.service.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
     public ResponseEntity<?> deleteAll() {
         System.out.println("Delete ALL");
-        return null;
+        this.service.deleteAll();
+        return ResponseEntity.ok().build();
     }
 }
